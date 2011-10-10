e715eadfe8b22da2a01d18433765489387be4f8c
co4222 <charles.ofarrell@mincom.com>
2011-10-11 08:50:43 +1000

    Some cleanup - easier to use from REPL

--- src/main/scala/org/charleso/util/JMap.scala
+++ src/main/scala/org/charleso/util/JMap.scala
@@ -10,40 +10,40 @@ import Scalaz._
 
 object JMap {
 
-  def main(args: Array[String]) = {
-    val maxLines = 100;
-    val instances = (i: (Int, Int)) => i._1
-    val bytes = (i: (Int, Int)) => i._2
-    val filter = if (args.contains("--instances")) instances else bytes
+  type Line = List[(Int, Int)]
 
-    val files = new File(args(0)).listFiles();
+  def main(args: Array[String]) = println(filterAll(parseAll(args(0)), instances).take(100).mkString("\n"))
 
-    def parse(f: File) = {
-      Source.fromFile(f).getLines()
-        .dropWhile(!_.contains(":"))
-        .toTraversable
-        .filter(!_.startsWith("Total"))
-        .map(parseLine(_))
-        .groupBy(_._1)
-        .mapValues(l => (l.map(_._2).sum, l.map(_._3).sum))
-        .mapValues(List(_))
-    }
-    val stuff = files.sorted.map(parse(_)).reduce(_ |+| _)
-      .filter(_._2.distinct.size != 1)
-      .mapValues(_.map(filter))
-      // What grows
-      .filter(a => a._2.sorted equals a._2)
-      .mapValues(a => (a.max - a.min) -> a).toList.sortBy(_._2._1).reverse
-      .take(maxLines)
+  def parseAll(dir: String) = new File(dir).listFiles().sorted
+    .par
+    .map(f => parseLines(Source.fromFile(f).getLines()))
+    .reduce(_ |+| _)
 
-    System.out.println(stuff.mkString("\n"))
+  def parseLines(lines: Iterator[String]) = {
+    lines.dropWhile(!_.contains(":"))
+      .filter(!_.startsWith("Total"))
+      .map(parseLine(_))
+      .toTraversable
+      .groupBy(_._1)
+      .mapValues(l => (l.map(_._2).sum, l.map(_._3).sum))
+      .mapValues(List(_))
   }
+
+  def filterAll(map: Map[String, Line], filter: ((Int, Int)) => Int) = map
+    .filter(_._2.distinct.size != 1)
+    .mapValues(_.map(filter))
+    .filter(a => a._2.sorted equals a._2)
+    .mapValues(a => (a.max - a.min) -> a).toList.sortBy(_._2._1).reverse
+
+  val instances = (i: (Int, Int)) => i._1
+
+  val bytes = (i: (Int, Int)) => i._2
 
   def parseLine(line: String) = {
     val a = line.replaceAll(" +", " ").trim.split(" ")
     (fixName(a(3)), a(1).toInt, a(2).toInt)
   }
- 
+
   def fixName(name: String) = {
     if (name.startsWith("sun.reflect.GeneratedMethodAccessor")) {
       "sun.reflect.GeneratedMethodAccessor"
